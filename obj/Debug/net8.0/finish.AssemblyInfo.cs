182ee059a1025b71964564c537eeb3e4b6867529
test <test@test>
2024-04-23 15:45:16 +0300

    add schema

--- obj/Debug/net8.0/finish.AssemblyInfo.cs
+++ obj/Debug/net8.0/finish.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("finish")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff005ec04066d6171c2d18415650f37044a9c73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d6cba3228bac09a30f41b897d366aa289f151d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("finish")]
 [assembly: System.Reflection.AssemblyTitleAttribute("finish")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
